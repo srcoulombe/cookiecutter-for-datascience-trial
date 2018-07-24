474213c8cae5a352f9032f713a7a1771d9df5c0a
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2018-07-24 09:09:42 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A cookiecutter project to play around with.',
+    author='srcoulombe',
+    license='MIT',
+)
